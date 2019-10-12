4b6e98affdebab4e8ae328074dcdfed42f35ee9d
unknown <fernanda.vc3399@gmail.com>
2019-10-12 18:03:53 -0400

    Primera subida

---
+++ CalculadoraBasica/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CalculadoraBasica
+{
+    class Program
+    {
+   
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+        Console.WriteLine("Inserte los numeros!");
+            Console.WriteLine("Inserte numero1");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserte numero2");
+            numero2 = int.Parse(Console.ReadLine());
+        }
+    }
+}
